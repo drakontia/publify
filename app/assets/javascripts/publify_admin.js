1542df502e938e3066302bcc9efcf530a26ff125
drakontia <mhden@drakontia.com>
2013-11-28 16:53:22 +0900

    Why delete publify_carousel.js refs #250 @1h

--- app/assets/javascripts/publify_admin.js
+++ app/assets/javascripts/publify_admin.js
@@ -3,6 +3,7 @@
 //= require jquery.ui.all
 //= require bootstrap
 //= require publify_jquery
+//= require publify_carousel
 //= require administration_jquery
 //= require_self
 //= require quicktags
