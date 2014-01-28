b090962f0786e2fb0a736f82954ead97aebd9111
Yannick Francois <yannick.francois@free.fr>
2014-01-28 22:28:34 +0100

    Remove more unused keys
    Conflicts:
    	lang/fr_FR.rb
    	lang/ja_JP.rb
    	lang/pt_BR.rb

--- lang/pt_BR.rb
+++ lang/pt_BR.rb
@@ -1,33 +1,11 @@
 # coding: utf-8
 # Translation by Edgar J. Suarez
 Localization.define("pt_BR") do |l|
-  l.store "%d trackbacks", "%d trackbacks"
-  l.store "no posts", "Sem posts"
-  l.store "Can't end in .rss or .atom. These are reserved to be used for feed URLs", "Não pode terminar em .rss ou .atom. Esses formatos são reservados para URLs de feed"
   l.store "Unclassified", "Não classificado"
   l.store "Just Presumed Ham", "Ham apenas presumido"
   l.store "Just Marked As Ham", "Apenas marcado como Ham"
   l.store "Just Marked As Spam", "Apenas marcado como Spam"
-  l.store "yes", "s&iacute;"
-  l.store "no", "no"
   l.store "Posted by", "Publicado por"
-  l.store "said", "dijo"
-  l.store "No articles found", "No se encontraron art&iacute;culos"
   l.store "is about to say", "est&aacute; a punto de decir"
-  l.store "There are", "Hay"
-  l.store "Use the following link to trackback from your own site", "Usa el siguiente link para crear un trackback desde tu propio sitio"
-  l.store "%d Articles", ["Art&iacute;culo", "%d Art&iacute;culos"]
-  l.store "%d Trackbacks", ["Trackback", "%d Trackbacks"]
-  l.store "Action", "Acci&oacute;n"
-  l.store "Activate", "Activar"
-  l.store "Body", "Comentario"
-  l.store "Markup", "Marcado"
-  l.store "Read", "Leer"
-  l.store "Read more", "Leer m&aacute;s"
-  l.store "String", "Cadena"
-  l.store "Textfilter", "Filtro de texto"
-  l.store "Type", "Tipo"
-  l.store "Publify admin", "Administrar Publify"
   l.store "by", "por"
-  l.store "on", "en"
 end
