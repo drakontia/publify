aecb2d77ec3b21cace6767f3f88189af2ec0110c
drakontia <mhden@drakontia.com>
2014-09-09 12:08:54 +0900

    Oops, I forget erasing js

--- app/assets/javascripts/publify_admin.js
+++ app/assets/javascripts/publify_admin.js
@@ -3,7 +3,6 @@
 //= require jquery.ui.all
 //= require bootstrap
 //= require publify_jquery
-//= require publify_carousel
 //= require administration_jquery
 //= require_self
 //= require quicktags
