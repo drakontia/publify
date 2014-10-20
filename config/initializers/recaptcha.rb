44cd6cbb15f3c4a5763641c3c0e4ccb0ed0714f7
drakontia <mhden@drakontia.com>
2014-10-20 12:04:06 +0900

    Set reCaptcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = 'YourAPIkeysHere_yyyyyyyyyyyyyyyyy'
-  config.private_key = 'YourAPIkeysHere_xxxxxxxxxxxxxxxxx'
+  config.public_key  = '6LfdSvwSAAAAAPDl2OxbjKfNW7MCCJL65ua6TOF5'
+  config.private_key = '6LfdSvwSAAAAAEi-PN4IIE2CEvuQKKjL5IAN3P9L'
 end
