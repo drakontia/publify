8545dbdd368d9669690a80c7251d5ea4260bdc55
drakontia <mhden@drakontia.com>
2017-01-10 11:05:41 +0900

    Update puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-require "active_record"
-cwd = File.dirname(__FILE__)+"/.."
-ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
-=======
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -51,4 +45,3 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
->>>>>>> dea539309f6bd7d7976807f53d873a78d5d4547d
