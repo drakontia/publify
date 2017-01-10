16678096d0970372c81e761838b636ba3c47801b
drakontia <mhden@drakontia.com>
2017-01-10 11:01:25 +0900

    Update recaptcha env.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LfdSvwSAAAAAPDl2OxbjKfNW7MCCJL65ua6TOF5'
-  config.private_key = '6LfdSvwSAAAAAEi-PN4IIE2CEvuQKKjL5IAN3P9L'
+  config.site_key  = '6LfdSvwSAAAAAPDl2OxbjKfNW7MCCJL65ua6TOF5'
+  config.secret_key = '6LfdSvwSAAAAAEi-PN4IIE2CEvuQKKjL5IAN3P9L'
 end
